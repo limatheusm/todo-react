6849dae5c5a9b0fa33d96b4f37f8b01dd00cd1f9
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-08-21 17:41:11 -0300

    Add frontend

---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+// Entry point
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public/',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loaders: 'babel-loader',
+            exclude: /node-modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            use: ['file-loader']
+        }]
+    }
+}
